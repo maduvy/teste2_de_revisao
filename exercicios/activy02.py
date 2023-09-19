501c0dcddaf9d6abd6d3a5acc798d3863ad7b032
maduvy <lourencomariaeduarda73@gmail.com>
2023-09-19 11:32:23 -0300

    activy02

---
+++ exercicios/activy02.py
@@ -0,0 +1,8 @@
+s =input('digite alguma coisa: ')
+print('tem espaço? ',s.isspace())
+print('é numerico? ',s.isnumeric())
+print('é alfabetico? ',s.isalpha())
+print('é alfanumerico? ',s.isalnum())
+print('é maiúscula? ',s.isupper())
+print('é minúscula? ' ,s.islower())
+
