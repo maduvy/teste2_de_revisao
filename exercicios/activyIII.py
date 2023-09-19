022f58299826acabaf48d9536fdd12415fb33793
maduvy <lourencomariaeduarda73@gmail.com>
2023-09-19 11:37:02 -0300

    exercicio 19/09

---
+++ exercicios/activyIII.py
@@ -0,0 +1,7 @@
+v1=int(input("digite o primeiro numero"))
+v2=int(input("digite o segundo numero"))
+s=v1+v2
+#aqui e feito o calculo da soma de dois numeros
+
+#print("e resultado entre:",v1, 'e',v2, 'e',s)
+print("o valor entre {} e o resultado e{}".format(v1,v2,s))
